df26d9a58d5cfe0a885ffc990bcc13a6326fc7bf
Amaralcode19 <lucasamaralap@outlook.com>
2023-05-30 10:36:07 -0300

    :wrench: minor-changes

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,6 +2,7 @@ import { Footer } from '@/components/Footer'
 import { Nav } from '@/components/Nav'
 import About from '@/components/About'
 import { Slider } from '@/components/Slider'
+
 export default function Main() {
   return (
     <div id="" className="flex flex-col items-center justify-center bg-white">
